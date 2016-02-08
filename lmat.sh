3af816cf6a2acfc9d1ad8333406128fe9753cd1e
gulinok <kmg549@nyu.edu>
2016-02-08 13:42:59 -0500

    Create lmat.sh

---
+++ lmat.sh
@@ -0,0 +1,45 @@
+# LMAT
+
+#before running:
+#Download "runtime_inputs" from ftp://gdo-bioinformatics.ucllnl.org/pub/lmat/
+#copy files from /share/apps/lmat/1.2.6/intel/bin to your own directory which you will use as the working directory
+#copy database files into this directory as well. They can be found here:/scratch/work/public/gen-data/lmat/runtime_inputs/ 
+
+	
+#!/bin/bash
+
+###-----Comments can be here as long as they're preceded by (#) hash-----###
+   #######------------and hash (#) is not followed by PBS------------#######
+
+###-----PBS Directives Start-----###
+
+#PBS -V
+#PBS -S /bin/bash
+#PBS -N lmat_run
+#PBS -l nodes=1:ppn=12
+#PBS -l walltime=24:00:00
+#PBS -l mem=92GB
+#PBS -q s48
+#PBS -M email@nyu.edu
+#PBS -m bea
+#PBS -e localhost:${PBS_O_WORKDIR}/${PBS_JOBNAME}.e${PBS_JOBID}
+#PBS -o localhost:${PBS_O_WORKDIR}/${PBS_JOBNAME}.o${PBS_JOBID}
+
+###-----PBS Directives End-----###
+
+###-----Statements to be Executed-----###
+
+cd path/to/working/directory
+module load kronatools/2.5
+module load seqtk/intel/1.0
+
+#LMAT_DIR=/scratch/kmg549/flu-metagenomics/example/runtime_inputs -- I downloaded the runtime inputs from ftp://gdo-bioinformatics.ucllnl.org/pub/lmat/ and place them here
+#LMAT_DIR=/path/to/runtimeinputs
+
+export LMAT_DIR=/path/to/runtimeinputs
+
+for i in {1..37}
+do
+BASE=${i}
+/share/apps/lmat/1.2.6/intel/bin/run_rl.sh --db_file=/path/to/dbfile --query_file=/path/to/query_file --odir=/path/to/outdirectory --threads=8
+done
